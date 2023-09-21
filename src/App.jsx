315023ffd83c7c8dc816dd54d5c5472185a28c23
unknown <daffaputrapratamaa@gmail.com>
2023-09-21 14:27:37 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, eius.
+      </h1>
+    </>
+  );
+}
+
+export default App;
